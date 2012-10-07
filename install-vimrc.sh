ca2ea16035327fdb735e648da5cea5fd9d17fcb7
chen bin <chenbin.sh@gmail.com>
2012-10-08 01:05:56 +1100

    avoid copying files in *nix environment

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 cd ..
 # hack to make gvim work on win32
-cp -r .vim/ vimfiles
-cp .vim/vimrc .vimrc
-cp .vim/gvimrc .gvimrc
+if [[ `uname -s` == *CYGWIN* ]]; then
+   cp -r .vim/ vimfiles
+   cp .vim/vimrc .vimrc
+   cp .vim/gvimrc .gvimrc
+else
+   ln -s .vim/vimrc .vimrc
+   ln -s .vim/gvimrc .gvimrc
+fi
 
 # xmledit
 cd .vim/bundle/xmledit/ftplugin/
-cp xml.vim html.vim
-cp xml.vim xhtml.vim
+if [[ `uname -s` == *CYGWIN* ]]; then
+   cp xml.vim html.vim
+   cp xml.vim xhtml.vim
+else
+   ln -s xml.vim html.vim
+   ln -s xml.vim xhtml.vim
+fi
