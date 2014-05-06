31f5dc8497024c6b2ce2412af36e4b8d44639a6e
Chen Bin <chenbin.sh@gmail.com>
2014-05-06 10:21:33 +0800

    vim set up

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -2,11 +2,12 @@
 
 # hack to make gvim work on win32
 if [ $(uname -s | grep -c CYGWIN) -eq 1 ]; then
-   cp -r ~/.vim/ ~/vimfiles
-   cp ~/.vim/vimrc ~/.vimrc
-   cp ~/.vim/gvimrc ~/.gvimrc
-   cp ~/.vim/vimrc ~/_vimrc
+    rm -rf ~/vimfiles ~/.vimrc ~/.gvimrc ~/_vimrc
+    cp -r ~/.vim/ ~/vimfiles
+    cp ~/.vim/vimrc ~/.vimrc
+    cp ~/.vim/gvimrc ~/.gvimrc
+    cp ~/.vim/vimrc ~/_vimrc
 else
-   ln -fs ~/.vim/vimrc ~/.vimrc
-   ln -fs ~/.vim/gvimrc ~/.gvimrc
+    ln -fs ~/.vim/vimrc ~/.vimrc
+    ln -fs ~/.vim/gvimrc ~/.gvimrc
 fi
